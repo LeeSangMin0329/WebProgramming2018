ce9675a901f167dc91627efd23df8a4d0020e3b4
DESKTOP-86OF63K\YOURI <dltkdals329@naver.com>
2018-03-18 19:14:28 +0900

    WebProj2 201310923 이상민

--- WebProgramingWithAspnet/WebNote.MVC6/Controllers/HomeController.cs
+++ WebProgramingWithAspnet/WebNote.MVC6/Controllers/HomeController.cs
@@ -39,5 +39,10 @@ namespace WebNote.MVC6.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Calculator()
+        {
+            return View();
+        }
     }
 }
